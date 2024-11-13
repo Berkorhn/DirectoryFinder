882018050e2b8e8393164a3546006b687fdf9d78
Berkorhn <berkorhan1406@gmail.com>
2024-11-14 00:54:33 +0300

    Directory Finder
    Directory finder with python

---
+++ DirectoryFinder/DirectoryFinder.py
@@ -0,0 +1,25 @@
+import requests
+
+def request(url):
+    try:
+        return requests.get("http://" + url)
+    except requests.exceptions.ConnectionError:
+        pass
+
+foundUrls = []
+
+def FindDirecties(url):
+    with open("wordlist.txt", "r") as listFile:
+        for line in listFile:
+            word = line.strip()
+            testUrl = url + "/" + word
+            response = request(testUrl)
+            if response:
+                print("Found URL --> " + testUrl)
+                foundUrls.append(word)
+
+url = "atilsamancioglu.com"
+FindDirecties(url)
+
+for foundUrl in foundUrls:
+    FindDirecties(url + "/" + foundUrl)
